1b8d577bffabe0d22ca533e2f477ac1354b93937
Tegar Sabila <94665690+tegarsabila@users.noreply.github.com>
2022-08-12 17:39:14 +0700

    Update Timer.js

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -43,7 +43,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("Oct 08, 2022 21:00:00").getTime();
+    this.deadline = new Date("Aug 12, 2022 17:43:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
